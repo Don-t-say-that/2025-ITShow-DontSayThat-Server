023c00693dd726e7c21cbff7eb8b1bacc013375d
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-07 02:03:27 +0900

    [feat] react에서 실행될 수 있도록 cors 허용

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // CORS 허용 react에서 실행될 수 있게 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
