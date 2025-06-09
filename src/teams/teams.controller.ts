58d855fcfa09ef433ac7377b48111bed56383dcd
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-09 17:02:10 +0900

    [feat] 게임룸 도메인 서비스 레이어 구현

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -1,8 +1,21 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { TeamsService } from './teams.service';
+import { CreateTeamDto } from './dto/create-team.dto';
+import { Team } from './entities/team.entity';
 
 @Controller('teams')
 export class TeamsController {
   constructor(private readonly teamsService: TeamsService) {}
+
+  @Post()
+  async createTeam(@Body() body: { name: string; leaderId: number }): Promise<Team> {
+    const { name, leaderId } = body;
+    return this.teamsService.createTeam(name, leaderId);
+  }
+
+  @Get('waiting')
+  async getWaitingTeams(): Promise<(Team & { userCount: number })[]> {
+    return this.teamsService.getWaitingTeams();
+  }
 
 }
