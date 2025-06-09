5d487089e4d9db3c0a10f49a5294da8d6f249372
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-09 17:03:34 +0900

    [chore] User entity 받아올 수 있도록 설정

--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Team } from './entities/team.entity';
 import { TeamsService } from './teams.service';
 import { TeamsController } from './teams.controller';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Team])],
+  imports: [
+    TypeOrmModule.forFeature([Team, User])
+  ],
   providers: [TeamsService],
   controllers: [TeamsController],
 })
